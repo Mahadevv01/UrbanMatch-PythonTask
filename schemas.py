959df6071092bd7e8c13f265f4ce8ffc541ac258
Mohit sharma <159415751+Mahadevv01@users.noreply.github.com>
2025-01-07 22:53:27 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,20 +1,27 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional
 
 class UserBase(BaseModel):
     name: str
     age: int
     gender: str
-    email: str
+    email: EmailStr  # Use EmailStr for email validation
     city: str
     interests: List[str]
 
 class UserCreate(UserBase):
     pass
 
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    gender: Optional[str] = None
+    email: Optional[EmailStr] = None  # Use EmailStr for email validation
+    city: Optional[str] = None
+    interests: Optional[List[str]] = None
+
 class User(UserBase):
     id: int
 
     class Config:
         orm_mode = True
-
